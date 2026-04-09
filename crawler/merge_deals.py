730b3f76755a8209ac3445666f2a99bf9a3c03d2
wony4860 <wony4860@gmail.com>
2026-04-09 17:03:22 +0900

    auto update deals

--- crawler/merge_deals.py
+++ crawler/merge_deals.py
@@ -7,8 +7,8 @@ site_path = base_path.parent / "site" / "deals.json"
 root_path = base_path.parent / "deals.json"
 
 sources = [
-    base_path / "products.json",
-    base_path / "api_products.json"
+    base_path / "api_products.json",
+    base_path / "products.json"
 ]
 
 rules_path = base_path / "rules.json"
